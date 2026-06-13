954423d482a9ab671548e6bc58e7cebe6b4763c8
Aarush-1234 <aarush56kk@gmail.com>
2026-06-13 18:56:04 +0530

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.mahamayamobiles.in',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}
